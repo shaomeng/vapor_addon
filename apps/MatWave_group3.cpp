6c23057d849994c7d0f7ce2f051fea7502ee5166
Samuel Li <shaomenglee@gmail.com>
2015-05-12 22:17:58 -0500

    MatWave_group3 is ready to test on many compression ratios at one time

--- apps/MatWave_group3.cpp
+++ apps/MatWave_group3.cpp
@@ -13,16 +13,21 @@ using namespace VAPoR;
 
 int main( int argc, char* argv[] )
 {
+    vector< int > cratios;
     if( argc < 2 ) {
         cerr << "Please specify the compression ratios to test!" << endl;
         exit(1);
     }
-    int ratio = stoi( argv[1] );
+    else
+        for( int i = 1; i < argc; i++ )
+            cratios.push_back( stoi( argv[i] ) );
+
     string wavename = "bior4.4";
     string* filenames = new string[ NSLICES ];
-
     struct timeval t_now;
     double timer1, timer2;
+
+
 /*
  * 504^3 test
  *
@@ -39,76 +44,93 @@ int main( int argc, char* argv[] )
         filenames[i] = path + "ru." + to_string(i) + ".float";
 
 
-/*
-    vector< int > cratios;
-    for( int i = 1; i < argc; i++ )
-        cratios.push_back( stoi( argv[i] ) );
-*/
 
     SamToolbox toolbox;
     SamSlice2** slices = new SamSlice2*[ NSLICES ];
-    float rms[ NSLICES ];
+    SamErr errs[ cratios.size() ][ NSLICES ];
 
     #pragma omp parallel for
     for( int i = 0; i < NSLICES; i++ ) {
         slices[i] = new SamSlice2( filenames[i], wavename, NX, NY, NZ );
         slices[i] -> Decompose();
-        slices[i] -> Reconstruct( ratio );
-        float* rawPtr = slices[i] -> GetRawPtr();
-        float* reconstructedPtr = slices[i] -> GetReconstructedPtr();
-        rms[i] = toolbox.CompareArrays( rawPtr, reconstructedPtr, NX*NY*NZ, false );
+    }
+
+    /* 
+     * Test on different cratios, 3D compression
+     */
+    #pragma omp parallel for
+    for( int i = 0; i < NSLICES; i++ ) 
+    {
+        for( int j = 0; j < cratios.size(); j++ )
+        {
+            int ratio = cratios[j];
+            slices[i] -> Reconstruct( ratio );
+            float* rawPtr = slices[i] -> GetRawPtr();
+            float* reconstructedPtr = slices[i] -> GetReconstructedPtr();
+            errs[j][i] = toolbox.CompareArrays( rawPtr, reconstructedPtr, NX*NY*NZ, false );
+        }
         slices[i] -> FreeReconstructed();   // make space for group operation
     }
-    // for( int i = 0; i < NSLICES; i++ )
-    //     cerr << "Slice# " << i << ", RMS=" << rms[i] << endl;
-    cerr << "\t==> 3D RMS: " << toolbox.CalcRMS( rms, NSLICES ) << endl;
+
+    cerr << "==> Slices from time stamp " << STARTIDX << endl;
+    cerr << "==> 3D compression on slices from " << endl;
+    cerr << "\tRatio,\t\tRMS,\t\t\tMAX" << endl;
+    for( int i = 0; i < cratios.size(); i++ ){
+        float max = toolbox.FindMax( errs[i], NSLICES );
+        float rms = toolbox.CalcRMS( errs[i], NSLICES );
+        cerr << "\t" << cratios[i] << ",\t\t" << rms << ",\t\t" << max << endl;
+    }
 
 
+    /*
+     * Setup Temporal Compression
+     */ 
     SamSliceGroup3* group = new SamSliceGroup3( wavename, NX*NY*NZ, NSLICES );
     vector< float* > rawarr;
     for( int i = 0; i < NSLICES; i++ )
         rawarr.push_back( slices[i] -> GetCoeffsPtr() );
     group -> UpdateRawPtr( rawarr );
 
-    gettimeofday(&t_now, NULL);
-    timer1 = t_now.tv_sec + (t_now.tv_usec/1000000.0);
+    //gettimeofday(&t_now, NULL);
+    //timer1 = t_now.tv_sec + (t_now.tv_usec/1000000.0);
     group -> Decompose();
-    gettimeofday(&t_now, NULL);
-    timer2 = t_now.tv_sec + (t_now.tv_usec/1000000.0);
-    cerr << "\tfinish temporal decomposing in " << (timer2 - timer1) << endl;
-
-    gettimeofday(&t_now, NULL);
-    timer1 = t_now.tv_sec + (t_now.tv_usec/1000000.0);
-    group -> Reconstruct( ratio );
-    gettimeofday(&t_now, NULL);
-    timer2 = t_now.tv_sec + (t_now.tv_usec/1000000.0);
-    cerr << "\tfinish temporal reconstruction in " << (timer2 - timer1) << endl;
+    //gettimeofday(&t_now, NULL);
+    //timer2 = t_now.tv_sec + (t_now.tv_usec/1000000.0);
+    //cerr << "\tfinish temporal decomposing in " << (timer2 - timer1) << endl;
 
 
+    /*
+     * Test on different cratios, 3D + 1D compression
+     */
+    for( int i = 0; i < cratios.size(); i++ )
+    {
+        group -> Reconstruct( cratios[i] );
 
-
-    #pragma omp parallel for
-    for( int i = 0; i < NSLICES; i++ ){
-        group -> FillReconstructedPtr( i, slices[i] -> GetCoeffsPtr() );
-        slices[i] -> Reconstruct( 1 );   
+        #pragma omp parallel for
+        for( int j = 0; j < NSLICES; j++ )
+        {
+            group -> FillReconstructedPtr( j, slices[j] -> GetCoeffsPtr() );
+            slices[j] -> Reconstruct( 1 );
+            float* rawPtr = slices[i] -> GetRawPtr();
+            float* reconstructedPtr = slices[i] -> GetReconstructedPtr();
+            errs[i][j] = toolbox.CompareArrays( rawPtr, reconstructedPtr, NX*NY*NZ, false );
+        }
     }
 
-    for( int i = 0; i < NSLICES; i++ ) {
-        float* rawPtr = slices[i] -> GetRawPtr();
-        float* reconstructedPtr = slices[i] -> GetReconstructedPtr();
-        assert( rawPtr != NULL );
-        assert( reconstructedPtr != NULL );
-        rms[i] = toolbox.CompareArrays( rawPtr, reconstructedPtr, NX*NY*NZ, false );
+    cerr << "==> 3D + 1D compression on slices from " << endl;
+    cerr << "\tRatio,\t\tRMS,\t\t\tMAX" << endl;
+    for( int i = 0; i < cratios.size(); i++ ){
+        float max = toolbox.FindMax( errs[i], NSLICES );
+        float rms = toolbox.CalcRMS( errs[i], NSLICES );
+        cerr << "\t" << cratios[i] << ",\t\t" << rms << ",\t\t" << max << endl;
     }
-    // for( int i = 0; i < NSLICES; i++ )
-    //     cerr << "Slice# " << i << ", RMS=" << rms[i] << endl;
-    cerr << "\t==> 3D+1D RMS: " << toolbox.CalcRMS( rms, NSLICES ) << endl;
 
-    
+
 
 
 
     if( group )                 delete group;
+
     for( int i = 0; i < NSLICES; i++ )
         if( slices[i] )         delete slices[i];    
     delete[]                    slices;
