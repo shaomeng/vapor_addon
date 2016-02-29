cd6ade7da26604282d273bfaa700e45f47c74c37
Samuel Li <shaomenglee@gmail.com>
2016-02-29 12:39:52 -0800

    testwavelet4d supports evaluation with different files

--- testwavelet4d.cpp
+++ testwavelet4d.cpp
@@ -6,11 +6,11 @@ using std::string;
 
 int main(int argc, char* argv[] )
 {
-	int startIdx = 1;
-	int total_steps = 90;
-	int each_group = 18;
+	int startIdx = 2;
+	int total_steps = 198;
+	int each_group = 9;
 	int cratio = 8;
-	string name = "xd";
+	string name = "viscosity";
 	if( argc == 2 )
 		cratio = atoi( argv[1] );
 	if( argc == 3 )
@@ -18,14 +18,17 @@ int main(int argc, char* argv[] )
 		cratio = atoi( argv[1] );
 		name = argv[2];
 	}
-	Wavelet4D wav( 63, 63, 63, each_group);
+	Wavelet4D wav( 96, 96, 96, each_group);
 	wav.SetCRatio( cratio );
-	string path = "/home/users/samuelli/Datasets/lulesh_field";
+	string path = "/home/users/samuelli/Datasets/cloverleaf/viscosity";
+	string pathCompare = "/home/users/samuelli/Datasets/Hurricane_w_special_values";
 	wav.SetPath( path );
+
 
 	for( int i = 0; i < total_steps / each_group; i++ )
 	{
 		wav.GenerateFilenames( name, i * each_group + startIdx );
+		wav.GenerateBkpFilenames( pathCompare, name, i * each_group + startIdx );
 		//wav.PrintFilenames();
 		cout << "start Idx: " << i * each_group + startIdx << endl;
 		wav.StartMonitor();
